7df99f769bb467aac3380641545115eba3960cdb
daniel <42454843+daniel@users.noreply.github.com>
2018-10-09 10:45:56 +0800

    Initial commit，10.9，图书购物开始

---
+++ src/main/java/wfy/jxnu/dao/BookDao.java
@@ -0,0 +1,22 @@
+package wfy.jxnu.dao;
+
+import wfy.jxnu.mo.Book;
+import wfy.jxnu.mo.OrderItem;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface BookDao {
+    public List<Book> getRecommadation(int size) throws SQLException;
+    public List<Book> getRecommThisWeek(int size) throws SQLException;
+    public List<Book> getRecommadation(int start,int size) throws SQLException;
+    //start记录的起始位置
+
+    public int getBookNumber() throws SQLException;
+    public Book findById(int id) throws SQLException;
+    // 添加订单后，修改商品的数量.
+    public boolean updateBookNum(List<OrderItem> items, Connection connection) throws SQLException;
+
+
+}
